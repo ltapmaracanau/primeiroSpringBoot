346b9f73ade5968c370397309f938c433d620f4f
Corneli Júnior <cjunior@ifce.edu.br>
2023-03-29 08:58:55 -0300

    inicial commit

---
+++ src/main/java/br/edu/ifce/primeiroSpringBoot/PrimeiroSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifce.primeiroSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroSpringBootApplication.class, args);
+	}
+
+}
