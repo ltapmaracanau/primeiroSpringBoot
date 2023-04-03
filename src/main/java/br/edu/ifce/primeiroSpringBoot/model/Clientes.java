cc51b1b8a11eb8527f8116b0f22227b8017a9eac
Corneli Júnior <cjunior@ifce.edu.br>
2023-04-03 09:00:48 -0300

    - Clientes

---
+++ src/main/java/br/edu/ifce/primeiroSpringBoot/model/Clientes.java
@@ -0,0 +1,47 @@
+package br.edu.ifce.primeiroSpringBoot.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tbl_clientes")
+public class Clientes {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "nomeDoCliente",nullable = false)
+	private String nome;
+	@Column(name = "numeroDoTelefone")
+	private String telefone;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+}
